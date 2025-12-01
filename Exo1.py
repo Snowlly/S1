2ffb5c9fa9a3b7e122a58f2ea0fbbca47abe9324
Snowlly <evanmartin2001@gmail.com>
2025-12-01 14:15:58 +0100

    MODIFY : add set()

--- Exo1.py
+++ Exo1.py
@@ -1,11 +1,7 @@
 def test_distinct(list):
-    r = []
-    n = len(list)
-    for i in range(n):
-        for j in range(n):
-            if (list[i] == list[j]):
-                r.append(list[i] == list[j])
-    return r
+    return len(list) == len(set(list))
+        
+
 
 print(test_distinct([1, 5, 7, 9]))
 print(test_distinct([2, 4, 5, 5, 7, 9]))
